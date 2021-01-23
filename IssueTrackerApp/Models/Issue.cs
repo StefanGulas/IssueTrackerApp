50199ee15f10565e2bcd58d44e6208bd7cacb4a8
stefan gulas <stefan.gulas@gmail.com>
2021-01-23 14:03:45 +0100

    Issue Model added

--- IssueTrackerApp/Models/Issue.cs
+++ IssueTrackerApp/Models/Issue.cs
@@ -9,6 +9,40 @@ namespace IssueTrackerApp.Models
     {
         public int Id { get; set; }
         public string IssueName { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Due { get; set; }
+        public enum IssueStatus
+        { 
+            Offen,
+            Bearbeitung,
+            Testen,
+            Wiedergeöffnet,
+            Angehalten,
+            Erledigt
+        }
+        public IssueStatus Status { get; set; }
+        public enum IssueSeverity
+        {
+            ShowStopper,
+            Kritisch,
+            Erheblich,
+            Unerheblich,
+            SpäterErledigen
+        }
+        public IssueSeverity Severity { get; set; }
+        public enum IssueIsRepoducible
+        {
+            Immer,
+            Manchmal,
+            Nie
+        }
+        public IssueIsRepoducible IsRepoducible { get; set; }
+        public string Reporter { get; set; }
+        public string Responsible { get; set; }
+        public string Supporting { get; set; }
+        public string Customer { get; set; }
+        public string Product { get; set; }
+        public string Team { get; set; }
 
     }
 }
