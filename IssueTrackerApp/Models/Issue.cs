cf49eda6d90988528dc1ffacf8a73ad3bb5f15a0
stefan gulas <stefan.gulas@gmail.com>
2021-01-23 09:49:22 +0100

    Projektdateien hinzufügen.

---
+++ IssueTrackerApp/Models/Issue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IssueTrackerApp.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        public string IssueName { get; set; }
+
+    }
+}
